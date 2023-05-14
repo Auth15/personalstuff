1ef1ab8db5e1d9754ac2ad24d1b8e0ab0aa75868
ginseng <101508816+Auth15@users.noreply.github.com>
2023-05-14 22:43:22 +0100

    Update LyricBot.lua

--- LyricBot.lua
+++ LyricBot.lua
@@ -1,41 +1,28 @@
-local GameInstance = game 
+local GameInstance = game; 
 
-local HttpService = GameInstance:GetService("HttpService")
-local ReplicatedStorage = GameInstance:GetService("ReplicatedStorage") 
+local HttpService = GameInstance:GetService("HttpService");
+local ReplicatedStorage = GameInstance:GetService("ReplicatedStorage"); 
 
-local RobloxChatEvents = ReplicatedStorage:FindFirstChild("DefaultChatSystemChatEvents") 
-local MsgRemote = RobloxChatEvents:FindFirstChild("SayMessageRequest")
+local RobloxChatEvents = ReplicatedStorage:FindFirstChild("DefaultChatSystemChatEvents"); 
+local MsgRemote = ReplicatedStorage:FindFirstChild("SayMessageRequest"); 
 
-if not getgenv().SingToggled then 
-	return 
-end 
+if not getgenv().SingToggled then return end 
 
-Title  = string.gsub(Title, " ", "")
-Artist = string.gsub(Artist, " ", "")
- 
-local SongData = HttpService:JSONDecode(game:HttpGet("https://lyrist.vercel.app/api/" .. Title .. "/" .. Artist))
+Title  = string.gsub(Title, " ", "");
+Artist = string.gsub(Artist, " ", "");
 
-if SongData.artist and SongData.title then 
-	local Lyrics = {} 
-	for Line in string.gmatch(SongData.lyrics, "[^\n]+") do 
-		table.insert(Lyrics, Line)
-	end
-	
-	game:GetService("StarterGui"):SetCore("SendNotification", {
-		Title = "Lyrist",
-		Text = "Singing " .. SongData.title .. " by " .. SongData.artist,
-		Duration = 5
-	})
-	
-	for i, Line in pairs(Lyrics) do 
-		if not getgenv().SingToggled then continue end 
-		MsgRemote:FireServer(Line, "All")
-		wait(MsgDelay)
+local SongJSON = HttpService:JSONDecode(game:HttpGet("https://lyrist.vercel.app/api/" .. Title .. "/" .. Artist)); 
+
+if SongJSON.artist and SongJSON.title then 
+	for Line in string.gmatch(SongJSON.lyrics, "[^\n]+") do 
+		if not getgenv().SingToggled then break end 
+		MsgRemote:FireServer(Line, "All");
+		wait(MsgDelay);
 	end
 else
 	game:GetService("StarterGui"):SetCore("SendNotification", {
 		Title = "Lyrist",
-		Text = "Lyrics not found for specified song and artist",
+		Text = "Lyrics not found for the specified artist and title",
 		Duration = 10 
-	})
+	});
 end 
