a607317ec565db2d1821ee51e7dfe71688e347b0
ginseng <101508816+Auth15@users.noreply.github.com>
2023-08-03 00:00:38 +0100

    Update sing for people.lua

--- sing for people.lua
+++ sing for people.lua
@@ -2,23 +2,33 @@ local GameInstance = game;
 
 local HttpService = GameInstance:GetService("HttpService");
 local ReplicatedStorage = GameInstance:GetService("ReplicatedStorage"); 
+local TextService = GameInstance:GetService("TextChatService"); 
 
 local RobloxChatEvents = ReplicatedStorage:FindFirstChild("DefaultChatSystemChatEvents"); 
-local MsgRemote = RobloxChatEvents:FindFirstChild("SayMessageRequest"); 
+local MsgRemote = RobloxChatEvents and RobloxChatEvents:FindFirstChild("SayMessageRequest"); 
 
 if not getgenv().SingToggled then return end 
+
+local function SendMessage(Msg)
+	if TextService.ChatInputBarConfiguration then 
+		TextService.ChatInputBarConfiguration.TargetTextChannel:SendAsync(Msg); 
+	else
+		MsgRemote:FireServer(Msg, "All"); 
+	end; 
+end; 
 
 getgenv().SaySpeed = getgenv().SaySpeed or 2.5  
 
 Title  = string.gsub(Title, " ", "");
 Artist = string.gsub(Artist, " ", "");
 
-local SongJSON = HttpService:JSONDecode(game:HttpGet("https://lyrist.vercel.app/api/" .. Title .. "/" .. Artist)); 
+local Result, SongJSON = game:HttpGet("https://lyrist.vercel.app/api/" .. Title .. "/" .. Artist); 
+SongJSON = HttpService:JSONDecode(Result); 
 
 if SongJSON.artist and SongJSON.title then 
 	for Line in string.gmatch(SongJSON.lyrics, "[^\n]+") do 
 		if not getgenv().SingToggled then break end 
-		MsgRemote:FireServer(Line, "All");
+		SendMessage(Line);
 		wait(SaySpeed);
 	end
 else
